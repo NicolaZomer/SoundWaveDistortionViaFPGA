2603d3910d3ed1ba8e0d24499c455fa60f72bf7a
Lorenzo Saccaro <lorenzo.saccaro@hotmail.it>
2022-02-13 23:05:10 +0100

    added a signal to test declipping + plot fft working as expected

--- audio_generator.py
+++ audio_generator.py
@@ -6,7 +6,7 @@ p = pyaudio.PyAudio()
 volume = 1     # range [0.0, 1.0]
 fs = 44100     # sampling rate, Hz, must be integer
 duration = 20.0   # in seconds, may be float
-f = 20.0        # sine frequency, Hz, may be float
+f = 400.0        # sine frequency, Hz, may be float
 
 # generate samples, note conversion to float32 array
 samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32) 
